70ca3ac5e81a6b9008f0ccc278f35380c0384558
MAYC 6976 <isayah@fungeapp.com>
2022-10-14 01:18:35 -0700

    added to design.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/* const getAverage = function (array) {
+  let total = 0;
+  array.forEach(function (value) {
+    total += value;
+  });
+  return total / array.length;
+};
+
+console.log(getAverage([1, 1, 1, 1, 1, 1])); */
+
+const testArray = [1, 2, 3, 4, 5];
+
+const getAverage = (array) =>
+  array.reduce((sum, value) => sum + value, 0) / array.length;
+
+console.log(`Average is: ${getAverage(testArray)}`);
