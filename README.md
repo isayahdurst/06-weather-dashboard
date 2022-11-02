0cad1eb98bf99657c2298841cb9c0d86eefc8bf4
MAYC 6976 <isayah@fungeapp.com>
2022-11-02 15:58:48 -0700

    finished feature to update recent searches. Completed readme.

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# 06-weather-dashboard
+# 06-Weather-Dashboard
+
+This application allows a user to search for a specific city and view the current and 5 day forcast for that city using the OpenWeather API. Searched cities are saved in recent searches for easy access.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+User Story:
+
+```
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+```
+
+Acceptance Criteria:
+
+```
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+```
+
+### Screenshot
+
+- Schedule Preview [Schedule](./assets/images/Screenshot.jpg?raw=true "Screenshot")
+
+### Links
+
+- Live Site URL: [Github](https://isayahdurst.github.io/06-weather-dashboard)
+
+## My process
+
+When I started the project, I began working with the OpenWeather API and making some calls in PostMan to get an understanding of how the data would be structured when it was returned to me. This proved helpful, and by mapping out the structure beforehand I was able to save a lot of time and needless API calls. After that, I began working on the HTML and CSS for the application and envisioned some very reactive features, such as dynamic background changes based on the time and weather conditions. Ultimately I wasn't able to add these features as a group-project I was working on simultaneously severely limited my time to work on this application, but if I were to return I would love to add the finishing touches on that. After getting the design close to how I wanted, the next step was to work on the JavaScript and make sure to manipulate the DOM according to the data that the API was returning. That was exciting and simple enough to achieve.
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS
+- JavaScript
+- Luxon
+
+### What I learned
+
+This project taught me how to correctly use Fetch API, along with asyncronous functions, to achieve requesting and manipulating data from a third party.
+
+### Continued development
+
+If I were to continue development, I would certainly improve the UI of this application and add the dynamic backgrounds which were aforementioned.
+
+## Author
+
+- Github - [@isayahdurst](https://www.github.com/isayahdurst)
+- Twitter - [@isayahdurst](https://www.twitter.com/isayahdurst)
+
+## Acknowledgments
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,228 +2,283 @@
 
 const DateTime = luxon.DateTime;
 
-const APIKey = '26adccd8ee2f8d64c1eff0ffe88e4fad';
-const openWeatherImgURL = 'https://openweathermap.org/img/wn/';
+const APIKey = "26adccd8ee2f8d64c1eff0ffe88e4fad";
+const openWeatherImgURL = "https://openweathermap.org/img/wn/";
 
-const searchBar = document.querySelector('#search');
-const searchBtn = document.querySelector('.search-btn');
-const searchResults = document.querySelector('.search-results');
+const searchBar = document.querySelector("#search");
+const searchBtn = document.querySelector(".search-btn");
+const searchResults = document.querySelector(".search-results");
 
 /* Current Day Summary Items */
-const currentCity = document.querySelector('.city-name');
-const currentTemp = document.querySelector('#summary-temp-val');
-const currentPrecip = document.querySelector('.summary-precip-val');
-const currentWind = document.querySelector('.summary-wind-val');
-const currentHumidity = document.querySelector('.summary-humidity-val');
-const currentCondition = document.querySelector('#summary-condition-img');
-const currentDay = document.querySelector('.city-label-weekday');
+const currentCity = document.querySelector(".city-name");
+const currentTemp = document.querySelector("#summary-temp-val");
+const currentPrecip = document.querySelector(".summary-precip-val");
+const currentWind = document.querySelector(".summary-wind-val");
+const currentHumidity = document.querySelector(".summary-humidity-val");
+const currentCondition = document.querySelector("#summary-condition-img");
+const currentDay = document.querySelector(".city-label-weekday");
 
 /* Forecast items */
-const forecastList = document.querySelector('#forecast-list');
+const forecastList = document.querySelector("#forecast-list");
 
 /* Modal Window Elements */
-const modalWindow = document.querySelector('.modal');
-const modalTitle = document.querySelector('.modal-title');
-const overlay = document.querySelector('.overlay');
-const closeModalBtn = document.querySelector('.close-modal');
-
+const modalWindow = document.querySelector(".modal");
+const modalTitle = document.querySelector(".modal-title");
+const overlay = document.querySelector(".overlay");
+const closeModalBtn = document.querySelector(".close-modal");
 
 const hideModal = function () {
-    modalWindow.classList.add('hidden');
-    overlay.classList.add('hidden');
-    searchResults.innerHTML = "";
+  modalWindow.classList.add("hidden");
+  overlay.classList.add("hidden");
+  searchResults.innerHTML = "";
 };
 
 const showModal = function () {
-    modalWindow.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
+  modalWindow.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
 const formatSearch = function (search) {
-    const newArr = search.split(',');
-    const formattedArr = newArr.map((element) => {
-        element = element.trim();
-        const tempElement = [...element];
-        tempElement.forEach(function(value, index) {
-            if (value === ' ') tempElement[index] = '-';
-        });
-        element = tempElement.join('');
-        return element;
-});
-    const formattedString = formattedArr.join(',');
-    return formattedString;
-}
+  const newArr = search.split(",");
+  const formattedArr = newArr.map((element) => {
+    element = element.trim();
+    const tempElement = [...element];
+    tempElement.forEach(function (value, index) {
+      if (value === " ") tempElement[index] = "-";
+    });
+    element = tempElement.join("");
+    return element;
+  });
+  const formattedString = formattedArr.join(",");
+  return formattedString;
+};
 
-const getWeatherData = async function (cityName, lat, lon, APIKey) {
-    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`);
-    const weatherData = await response.json();
-    sections[0].classList.remove('hidden');
-    sections[1].classList.remove('hidden');
-    console.log(weatherData);
-    searchBar.value = '';
-    hideModal();
-    currentCity.textContent = cityName;
-    currentTemp.innerHTML = `${Math.round(weatherData.daily[0].temp.day)} &#8457`;
-    if (!isNaN(weatherData.daily[0].rain)) {
-        currentPrecip.textContent = `${(weatherData.daily[0].rain / 25.4).toFixed(2)}"`;
-    } else {
-        currentPrecip.textContent = '0"';
-    }
+const savedCities = JSON.parse(localStorage.getItem("recentSearches")) || {};
 
-    currentWind.textContent = `${weatherData.daily[0].wind_speed} MPH`;
-    currentHumidity.textContent = `${weatherData.daily[0].humidity}%`;
-    currentCondition.src = `${openWeatherImgURL}${weatherData.daily[0].weather[0].icon}@2x.png`;
-    currentCondition.alt = `${weatherData.daily[0].weather[0].description}`;
-    currentCondition.title = `${weatherData.daily[0].weather[0].description}`;
-    currentDay.textContent = getWeekday(weatherData.daily[0].dt, {zone: weatherData.timezone});
+const getWeatherData = async function (cityName, lat, lon) {
+  const response = await fetch(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=${APIKey}`
+  );
+  const weatherData = await response.json();
+  sections[0].classList.remove("hidden");
+  sections[1].classList.remove("hidden");
+  console.log(weatherData);
+  searchBar.value = "";
+  hideModal();
+  currentCity.textContent = cityName;
+  currentTemp.innerHTML = `${Math.round(weatherData.daily[0].temp.day)} &#8457`;
+  if (!isNaN(weatherData.daily[0].rain)) {
+    currentPrecip.textContent = `${(weatherData.daily[0].rain / 25.4).toFixed(
+      2
+    )}"`;
+  } else {
+    currentPrecip.textContent = '0"';
+  }
 
-    renderForcast(weatherData.daily, weatherData.timezone);
-}
+  currentWind.textContent = `${weatherData.daily[0].wind_speed} MPH`;
+  currentHumidity.textContent = `${weatherData.daily[0].humidity}%`;
+  currentCondition.src = `${openWeatherImgURL}${weatherData.daily[0].weather[0].icon}@2x.png`;
+  currentCondition.alt = `${weatherData.daily[0].weather[0].description}`;
+  currentCondition.title = `${weatherData.daily[0].weather[0].description}`;
+  currentDay.textContent = getWeekday(weatherData.daily[0].dt, {
+    zone: weatherData.timezone,
+  });
+
+  renderForcast(weatherData.daily, weatherData.timezone);
+  savedCities[cityName] = weatherData;
+  localStorage.setItem("recentSearches", JSON.stringify(savedCities));
+  renderRecentSearches();
+};
 
 const renderForcast = function (weatherArray, zone) {
-    
-    forecastList.innerHTML = '';
+  forecastList.innerHTML = "";
 
-    weatherArray.forEach(function(value, index) {
-        if (index != 0 && index != 7) {
-            const day = document.createElement('li');
-            const wrapper = document.createElement('div');
-            wrapper.classList.add('card-wrapper');
+  weatherArray.forEach(function (value, index) {
+    if (index != 0 && index != 7) {
+      const day = document.createElement("li");
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("card-wrapper");
 
-            // SETS THE DAY
-            const time = document.createElement('div');
-            time.classList.add('time');
-            time.classList.add('large');
-            time.textContent = `${getWeekday(value.dt, {zone: zone})}`;
-            
-            // ADDS WEATHER IMAGE, ALT, AND TITLE (tipbox)
-            const condition = document.createElement('div');
-            condition.classList.add('time-period-condition');
-            const image = document.createElement('img');
-            image.src = `${openWeatherImgURL}${value.weather[0].icon}.png`;
-            image.alt, image.title = value.weather[0].description;
-            condition.appendChild(image);
+      // SETS THE DAY
+      const time = document.createElement("div");
+      time.classList.add("time");
+      time.classList.add("large");
+      time.textContent = `${getWeekday(value.dt, { zone: zone })}`;
 
-            // ADDS TEMPERATURE
-            const tempIcon = document.createElement('i');
-            tempIcon.classList.add('fa-solid', 'fa-temperature-three-quarters');
-            const temp = document.createElement('div');
-            temp.append(tempIcon);
-            temp.innerHTML += ` ${Math.round(value.temp.day)} F`
+      // ADDS WEATHER IMAGE, ALT, AND TITLE (tipbox)
+      const condition = document.createElement("div");
+      condition.classList.add("time-period-condition");
+      const image = document.createElement("img");
+      image.src = `${openWeatherImgURL}${value.weather[0].icon}.png`;
+      image.alt, (image.title = value.weather[0].description);
+      condition.appendChild(image);
 
-            // ADDS RAIN
-            const precipitation = document.createElement('div');
-            const rainIcon = document.createElement('i');
-            rainIcon.classList.add('fa-regular', 'fa-droplet');
-            precipitation.append(rainIcon);
-            if (!isNaN(value.rain)) {
-                precipitation.innerHTML += ` ${(value.rain / 25.4).toFixed(2)}"`;
-            } else {
-                precipitation.innerHTML += ' 0"';
-            }
+      // ADDS TEMPERATURE
+      const tempIcon = document.createElement("i");
+      tempIcon.classList.add("fa-solid", "fa-temperature-three-quarters");
+      const temp = document.createElement("div");
+      temp.append(tempIcon);
+      temp.innerHTML += ` ${Math.round(value.temp.day)} F`;
 
-            // ADDS WIND SPEED 
-            const windIcon = document.createElement('i');
-            windIcon.classList.add('fa-solid', 'fa-wind');
-            const windSpeed = document.createElement('div');
-            windSpeed.append(windIcon);
-            windSpeed.innerHTML += ` ${value.wind_speed} MPH`;
+      // ADDS RAIN
+      const precipitation = document.createElement("div");
+      const rainIcon = document.createElement("i");
+      rainIcon.classList.add("fa-regular", "fa-droplet");
+      precipitation.append(rainIcon);
+      if (!isNaN(value.rain)) {
+        precipitation.innerHTML += ` ${(value.rain / 25.4).toFixed(2)}"`;
+      } else {
+        precipitation.innerHTML += ' 0"';
+      }
 
-            // ADDS HUMIDITY
-            const humidityIcon = document.createElement('i');
-            humidityIcon.classList.add('fa-regular', 'fa-droplet-percent');
-            const humidity = document.createElement('div');
-            humidity.append(humidityIcon);
-            humidity.innerHTML += ` ${value.humidity} %`;
+      // ADDS WIND SPEED
+      const windIcon = document.createElement("i");
+      windIcon.classList.add("fa-solid", "fa-wind");
+      const windSpeed = document.createElement("div");
+      windSpeed.append(windIcon);
+      windSpeed.innerHTML += ` ${value.wind_speed} MPH`;
 
-            forecastList.appendChild(day);
-            day.appendChild(wrapper);
-            const wrapperChildren = [time, condition, temp, precipitation, windSpeed, humidity];
+      // ADDS HUMIDITY
+      const humidityIcon = document.createElement("i");
+      humidityIcon.classList.add("fa-regular", "fa-droplet-percent");
+      const humidity = document.createElement("div");
+      humidity.append(humidityIcon);
+      humidity.innerHTML += ` ${value.humidity} %`;
 
-            wrapperChildren.forEach(function (value) {
-                wrapper.appendChild(value);
-            })
-        }
-    })
-}
+      forecastList.appendChild(day);
+      day.appendChild(wrapper);
+      const wrapperChildren = [
+        time,
+        condition,
+        temp,
+        precipitation,
+        windSpeed,
+        humidity,
+      ];
+
+      wrapperChildren.forEach(function (value) {
+        wrapper.appendChild(value);
+      });
+    }
+  });
+};
 
 const getWeekday = function (timeInSeconds, zone) {
-    const weekdays = {
-        '1': 'Monday',
-        '2': 'Tuesday',
-        '3': 'Wednesday',
-        '4': 'Thursday',
-        '5': 'Friday',
-        '6': 'Saturday',
-        '7': 'Sunday'
-    }
-    const time = DateTime.fromSeconds(timeInSeconds, zone);
-    const timeISO = time.toISO();
-    const weekday = time.weekday;
-    return weekdays[weekday];
-}
+  const weekdays = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday",
+  };
+  const time = DateTime.fromSeconds(timeInSeconds, zone);
+  const timeISO = time.toISO();
+  const weekday = time.weekday;
+  return weekdays[weekday];
+};
 
 const createCitiesList = function (cities) {
-    if (cities.length) {
-        modalTitle.textContent = `Please select a city:`;
-        modalTitle.classList.remove('error');
-        
-        cities.forEach(function(value, index) {
-            const result = document.createElement('li');
-            result.textContent = `${value.name}, ${value.state}, ${value.country}`;
-            result.setAttribute('data-lat', value.lat);
-            result.setAttribute('data-lon', value.lon);
-            searchResults.appendChild(result);
-        });
-    } else {
-        modalTitle.textContent = `Sorry, no results found. Please refine your search and try again.`;
-        modalTitle.classList.add('error');
-    }
-}
+  if (cities.length) {
+    modalTitle.textContent = `Please select a city:`;
+    modalTitle.classList.remove("error");
+
+    cities.forEach(function (value, index) {
+      const result = document.createElement("li");
+      result.textContent = `${value.name}, ${value.state}, ${value.country}`;
+      result.setAttribute("data-lat", value.lat);
+      result.setAttribute("data-lon", value.lon);
+      searchResults.appendChild(result);
+    });
+  } else {
+    modalTitle.textContent = `Sorry, no results found. Please refine your search and try again.`;
+    modalTitle.classList.add("error");
+  }
+};
 
 const getCities = async function (search, limit, APIKey) {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=${limit}&appid=${APIKey}`);
-    const cityArray = await response.json();
-    showModal();
-    createCitiesList(cityArray);
-}
+  const response = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=${limit}&appid=${APIKey}`
+  );
+  const cityArray = await response.json();
+  showModal();
+  createCitiesList(cityArray);
+};
 
 /* Event Listeners */
 
-searchBtn.addEventListener('click', function () {
-    getCities(formatSearch(searchBar.value), 5, APIKey);
+searchBtn.addEventListener("click", function () {
+  getCities(formatSearch(searchBar.value), 5, APIKey);
 });
 
-closeModalBtn.addEventListener('click', hideModal);
-overlay.addEventListener('click', hideModal);
+closeModalBtn.addEventListener("click", hideModal);
+overlay.addEventListener("click", hideModal);
 
-searchResults.addEventListener('click', function (event) {
-    const target = event.target;
-    if (event.target.tagName.toLowerCase() === 'li') {
-        const lon = target.getAttribute('data-lon');
-        const lat = target.getAttribute('data-lat');
-        const cityName = target.textContent;
-        modalWindow.classList.add('hidden');
-        getWeatherData(cityName, lat, lon, APIKey);
-        // Summon WeatherData with correct LAT LON values
-    }
-})
+searchResults.addEventListener("click", function (event) {
+  const target = event.target;
+  if (event.target.tagName.toLowerCase() === "li") {
+    const lon = target.getAttribute("data-lon");
+    const lat = target.getAttribute("data-lat");
+    const cityName = target.textContent;
+    modalWindow.classList.add("hidden");
+    getWeatherData(cityName, lat, lon);
+    // Summon WeatherData with correct LAT LON values
+  }
+});
 
-// Set Background Image: 
+// Set Background Image:
 
 console.log(DateTime.now());
 
-const sections = document.querySelectorAll('.weather-summary');
+const sections = document.querySelectorAll(".weather-summary");
 
-sections[0].classList.add('hidden');
-sections[1].classList.add('hidden');
+sections[0].classList.add("hidden");
+sections[1].classList.add("hidden");
 
-const findDupe = function (array) {
+const renderRecentSearches = function () {
+  const recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
+  const recentSearchesList = document.querySelector(".recent-searches-list");
+  recentSearchesList.innerHTML = "";
+  for (const city of Object.keys(recentSearches)) {
+    const cityLI = document.createElement("li");
+    cityLI.classList.add("recent-city");
+    const cityLIName = document.createElement("span");
+    cityLI.setAttribute("data-city", city);
+    cityLI.appendChild(cityLIName);
+    cityLIName.textContent = city;
+    recentSearchesList.appendChild(cityLI);
+  }
 
-    const dupeNumbers = [];
-    array.forEach(function(value, index, array) {
-        if (indexOf(array) > 1 ) {
-            dupeNumbers.push(value);
-        }
-    })
-}
+  const cityLI = document.querySelectorAll(".recent-city");
+
+  cityLI.forEach(function (city) {
+    city.addEventListener("click", function (event) {
+      console.log("been clicked");
+      const cityName = event.currentTarget.getAttribute("data-city");
+      const cities = JSON.parse(localStorage.getItem("recentSearches"));
+      const { lat, lon } = cities[cityName];
+      getWeatherData(cityName, lat, lon);
+    });
+  });
+};
+
+renderRecentSearches();
+
+/* const cityLI = document.querySelectorAll(".recent-city");
+
+cityLI.forEach(function (city) {
+  city.addEventListener("click", function (event) {
+    console.log("been clicked");
+    const cityName = event.currentTarget.getAttribute("data-city");
+    const cities = JSON.parse(localStorage.getItem("recentSearches"));
+    const { lat, lon } = cities[cityName];
+    getWeatherData(cityName, lat, lon);
+  });
+}); */
+
+const showLS = function () {
+  console.log(Object.keys(JSON.parse(localStorage.getItem("recentSearches"))));
+};
+
+showLS();
